f238fbfaf1f524319cce02e357498b6784041a97
ykris <yuans@rose-hulman>
2018-02-01 03:50:28 -0500

    Done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -25,9 +25,7 @@ class Snatch3r(object):
         self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
         self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
         self.arm_motor = ev3.LargeMotor(ev3.OUTPUT_A)
-        assert self.left_motor.connected
-        assert self.right_motor.connected
-        assert self.arm_motor.connected
+
 
     def drive_inches(self, position, speed):
         if position < 0:
@@ -39,9 +37,9 @@ class Snatch3r(object):
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
     def turn_degrees(self, degrees_to_turn, turn_speed_sp):
-        self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=-degrees_to_turn,
+        self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=-degrees_to_turn*450/90,
                                        stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-        self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn,
+        self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp, position_sp=degrees_to_turn*450/90,
                                         stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
@@ -61,7 +59,7 @@ class Snatch3r(object):
         self.arm_motor.position = 0
 
     def arm_up(self, touch_sensor):
-        self.arm_motor.run_to_rel_pos(position_sp=14.2*360, speed_sp=)
+        self.arm_motor.run_to_rel_pos(position_sp=14.2*360, speed_sp=900)
         while touch_sensor.is_pressed:
             time.sleep(0.01)
         self.arm_motor.stop()
