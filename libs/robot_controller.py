7dce61cf4474144feb08d1bce9e7081eba634657
ykris <yuans@rose-hulman>
2018-02-07 11:07:21 -0500

    Done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -37,9 +37,9 @@ class Snatch3r(object):
     def drive_inches(self, distance, speed):
         assert self.left_motor.connected
         assert self.right_motor.connected
-        if position < 0:
+        if distance < 0:
             speed = -speed
-            distance = position * 90
+            distance = distance * 90
             self.left_motor.run_to_rel_pos(speed_sp=speed, position_sp=distance, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
             self.right_motor.run_to_rel_pos(speed_sp=speed, position_sp=distance, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
             self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
