7134b14a2aba9573565e8db77bf2d35a4391d0fa
donge1114 <donge@rose-hulman.edu>
2018-02-12 00:34:53 -0500

    Change Mr. One to dongbaba

--- sandbox/dongbaba/analog_sensors/ir_sensor/m1_beep_at_hands.py
+++ sandbox/dongbaba/analog_sensors/ir_sensor/m1_beep_at_hands.py
@@ -36,6 +36,9 @@ def main():
         #   self.ir_sensor = ev3.InfraredSensor()
         #   assert self.ir_sensor
         # Then here you can use a command like robot.ir_sensor.proximity
+        if robot.ir_sensor.proximity < 10:
+            ev3.Sound().beep().wait(1.5)
+        print(robot.ir_sensor.proximity)
 
         time.sleep(0.1)
 
